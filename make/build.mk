76096a6531ec8611b0bdf8545a5616e04ee9f6b3
Toni Liesche <dev@toniliesche.de>
2025-03-11 11:48:06 +0100

    updated build

--- make/build.mk
+++ make/build.mk
@@ -14,6 +14,7 @@ build-docker-rc: set-version-rc set-commit
 		--build-arg GOLANGVER=$(GOLANGVER) \
 		-t tliesche/powerdns-auth-proxy:$(run.commit) \
 		-t tliesche/powerdns-auth-proxy:$(run.build.version) \
+		-t tliesche/powerdns-auth-proxy:edge \
 		$(if $(PUSH),--push,--no-cache --progress=plain) \
 		docker/auth-proxy
 
